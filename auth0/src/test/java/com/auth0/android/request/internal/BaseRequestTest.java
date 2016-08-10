ef593435615bc74b3d32112bac2abb746c428c8f
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-08-10 12:35:49 -0300

    test BaseRequest and AuthenticationErrorBuilder

--- auth0/src/test/java/com/auth0/android/request/internal/BaseRequestTest.java
+++ auth0/src/test/java/com/auth0/android/request/internal/BaseRequestTest.java
@@ -27,15 +27,22 @@ package com.auth0.android.request.internal;
 
 import com.auth0.android.Auth0Exception;
 import com.auth0.android.RequestBodyBuildException;
+import com.auth0.android.authentication.ParameterBuilder;
 import com.auth0.android.callback.BaseCallback;
 import com.auth0.android.request.ErrorBuilder;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Protocol;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.collection.IsMapContaining;
+import org.hamcrest.collection.IsMapWithSize;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -44,9 +51,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
@@ -64,36 +76,20 @@ public class BaseRequestTest {
     private OkHttpClient client;
     @Mock
     private TypeAdapter<String> adapter;
+    @Mock
+    private Map<String, String> headers;
+    private ParameterBuilder parameterBuilder;
+
     @Captor
-    private ArgumentCaptor<Runnable> captor;
+    private ArgumentCaptor<Map<String, Object>> mapCaptor;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         HttpUrl url = HttpUrl.parse("https://auth0.com");
-        errorBuilder = new ErrorBuilder<Auth0Exception>() {
-            @Override
-            public Auth0Exception from(String message) {
-                return new Auth0Exception(message);
-            }
+        parameterBuilder = ParameterBuilder.newBuilder();
 
-            @Override
-            public Auth0Exception from(String message, Auth0Exception exception) {
-                return exception;
-            }
-
-            @Override
-            public Auth0Exception from(Map<String, Object> values) {
-                return new Auth0Exception("Error");
-            }
-
-            @Override
-            public Auth0Exception from(String payload, int statusCode) {
-                return new Auth0Exception(payload);
-            }
-        };
-
-        baseRequest = new BaseRequest<String, Auth0Exception>(url, client, new Gson(), adapter, errorBuilder, callback) {
+        baseRequest = new BaseRequest<String, Auth0Exception>(url, client, new Gson(), adapter, errorBuilder, callback, headers, parameterBuilder) {
             @Override
             public String execute() throws Auth0Exception {
                 return null;
@@ -112,6 +108,62 @@ public class BaseRequestTest {
     }
 
     @Test
+    public void shouldUpdateTheCallback() throws Exception {
+        BaseCallback<String, Auth0Exception> diffCallback = mock(BaseCallback.class);
+        baseRequest.setCallback(diffCallback);
+        assertThat(baseRequest.getCallback(), CoreMatchers.equalTo(diffCallback));
+    }
+
+    @Test
+    public void shouldGetTheCallback() throws Exception {
+        assertThat(baseRequest.getCallback(), CoreMatchers.equalTo(callback));
+    }
+
+    @Test
+    public void shouldGetTheErrorBuilder() throws Exception {
+        assertThat(baseRequest.getErrorBuilder(), CoreMatchers.equalTo(errorBuilder));
+    }
+
+    @Test
+    public void shouldGetTheAdapter() throws Exception {
+        assertThat(baseRequest.getAdapter(), CoreMatchers.equalTo(adapter));
+    }
+
+    @Test
+    public void shouldAddHeaders() throws Exception {
+        baseRequest.addHeader("name", "value");
+        verify(headers).put("name", "value");
+    }
+
+    @Test
+    public void shouldAddASingleParameter() throws Exception {
+        baseRequest.addParameter("name", "value");
+
+        final Map<String, Object> result = parameterBuilder.asDictionary();
+        assertThat(result, IsMapWithSize.aMapWithSize(1));
+        assertThat(result, IsMapContaining.hasEntry("name", (Object) "value"));
+    }
+
+    @Test
+    public void shouldAddParameters() throws Exception {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "value");
+        params.put("asd", "123");
+        baseRequest.addParameters(params);
+
+        final Map<String, Object> result = parameterBuilder.asDictionary();
+        assertThat(result, IsMapWithSize.aMapWithSize(2));
+        assertThat(result, IsMapContaining.hasEntry("name", (Object) "value"));
+        assertThat(result, IsMapContaining.hasEntry("asd", (Object) "123"));
+    }
+
+    @Test
+    public void shouldSetBearer() throws Exception {
+        baseRequest.setBearer("my-jwt-token");
+        verify(headers).put("Authorization", "Bearer my-jwt-token");
+    }
+
+    @Test
     public void shouldPostOnSuccess() throws Exception {
         baseRequest.postOnSuccess("OK");
         verify(callback).onSuccess(eq("OK"));
@@ -123,5 +175,75 @@ public class BaseRequestTest {
         baseRequest.postOnFailure(throwable);
         verify(callback).onFailure(eq(throwable));
         verifyNoMoreInteractions(callback);
+    }
+
+    @Test
+    public void shouldBuildException() throws Exception {
+        baseRequest.onFailure(null, mock(IOException.class));
+        verify(errorBuilder).from(eq("Request failed"), any(Auth0Exception.class));
+    }
+
+    @Test
+    public void shouldParseUnsuccessfulJsonResponse() throws Exception {
+        String payload = "{key: \"value\", asd: \"123\"}";
+        final Response response = createJsonResponse(payload, 401);
+        baseRequest.parseUnsuccessfulResponse(response);
+
+        verify(errorBuilder).from(mapCaptor.capture());
+        assertThat(mapCaptor.getValue(), IsMapContaining.hasEntry("key", (Object) "value"));
+        assertThat(mapCaptor.getValue(), IsMapContaining.hasEntry("asd", (Object) "123"));
+    }
+
+    @Test
+    public void shouldParseUnsuccessfulNotJsonResponse() throws Exception {
+        String payload = "n=ot_a valid json {{]";
+        final Response response = createJsonResponse(payload, 401);
+        baseRequest.parseUnsuccessfulResponse(response);
+
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> integerCaptor = ArgumentCaptor.forClass(Integer.class);
+        verify(errorBuilder).from(stringCaptor.capture(), integerCaptor.capture());
+        assertThat(integerCaptor.getValue(), is(401));
+        assertThat(stringCaptor.getValue(), is("n=ot_a valid json {{]"));
+    }
+
+    @Test
+    public void shouldParseUnsuccessfulInvalidResponse() throws Exception {
+        byte[] invalidBytes = new byte[]{12,23,2,1,23,3,21,3,12};
+        final Response response = createBytesResponse(invalidBytes, 401);
+        response.body().string();    //force a IOException the next time this gets called
+        baseRequest.parseUnsuccessfulResponse(response);
+
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        verify(errorBuilder).from(stringCaptor.capture(), any(Auth0Exception.class));
+        assertThat(stringCaptor.getValue(), is("Request to https://auth0.com/ failed"));
+    }
+
+    private Response createJsonResponse(String jsonPayload, int code) {
+        Request request = new Request.Builder()
+                .url("https://someurl.com")
+                .build();
+
+        final ResponseBody responseBody = ResponseBody.create(MediaType.parse("application/json; charset=utf-8"), jsonPayload);
+        return new Response.Builder()
+                .request(request)
+                .protocol(Protocol.HTTP_1_1)
+                .body(responseBody)
+                .code(code)
+                .build();
+    }
+
+    private Response createBytesResponse(byte[] content, int code) {
+        Request request = new Request.Builder()
+                .url("https://someurl.com")
+                .build();
+
+        final ResponseBody responseBody = ResponseBody.create(MediaType.parse("application/octet-stream; charset=utf-8"), content);
+        return new Response.Builder()
+                .request(request)
+                .protocol(Protocol.HTTP_1_1)
+                .body(responseBody)
+                .code(code)
+                .build();
     }
 }
