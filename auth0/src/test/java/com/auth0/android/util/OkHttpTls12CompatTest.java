99f2be0be63308d6f5b2dd66967bf1ee229ab2e4
Leo Ma <Leo.ma@dowjones.com>
2017-11-06 16:01:07 +0800

    Add tests on TLS support utils.

---
+++ auth0/src/test/java/com/auth0/android/util/OkHttpTls12CompatTest.java
@@ -0,0 +1,75 @@
+package com.auth0.android.util;
+
+import android.app.Activity;
+
+import com.squareup.okhttp.ConnectionSpec;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.TlsVersion;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.List;
+
+import javax.net.ssl.SSLSocketFactory;
+
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = com.auth0.android.auth0.BuildConfig.class, sdk = 21, manifest = Config.NONE)
+public class OkHttpTls12CompatTest {
+
+    Activity activity;
+    @Mock OkHttpClient client;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        activity = Robolectric.setupActivity(Activity.class);
+    }
+
+    @Test
+    @Config(sdk=22)
+    public void shouldNotConfigTlsPostApi21() {
+        OkHttpTls12Compat.enableSupportOnPreLollipop(client);
+        verify(client, never()).setSslSocketFactory((SSLSocketFactory) any());
+    }
+
+    @Test
+    public void shouldConfigTlsOnOrPreApi21() {
+        OkHttpTls12Compat.enableSupportOnPreLollipop(client);
+
+        ArgumentCaptor<SSLSocketFactory> factoryCaptor = ArgumentCaptor.forClass(SSLSocketFactory.class);
+        verify(client).setSslSocketFactory(factoryCaptor.capture());
+        assertTrue(factoryCaptor.getValue() instanceof Tls12SocketFactory);
+
+        ArgumentCaptor<List> specCaptor = ArgumentCaptor.forClass(List.class);
+        verify(client).setConnectionSpecs(specCaptor.capture());
+        boolean hasTls12 = false;
+        for (Object item : specCaptor.getValue()) {
+            assertTrue(item instanceof ConnectionSpec);
+            ConnectionSpec spec = (ConnectionSpec) item;
+            if (!spec.isTls()) {
+                continue;
+            }
+            List<TlsVersion> versions = spec.tlsVersions();
+            for (TlsVersion version : versions) {
+                if ("TLSv1.2".equals(version.javaName())) {
+                    hasTls12 = true;
+                    break;
+                }
+            }
+        }
+        assertTrue(hasTls12);
+    }
+}
