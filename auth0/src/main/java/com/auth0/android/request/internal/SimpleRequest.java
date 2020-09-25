2b92ae34a094393c3653ada7df138a50b5ef2014
Jeremy Klein <jeremy.klein@quibi.com>
2020-09-25 12:21:08 -0700

    Catch a gson JsonIOException when parsing SimpleRequest response.
    IOExceptions are already caught, but it turns out in some cases, gson actually wraps underlying IOExceptions in JsonIOExceptions (which does not extent IOException).
    
    Example stack trace:
    ```
    Fatal Exception: com.google.gson.JsonIOException: java.net.SocketTimeoutException: timeout
       at com.google.gson.internal.Streams.parse(Streams.java:62)
       at com.google.gson.internal.bind.TreeTypeAdapter.read(TreeTypeAdapter.java:65)
       at com.google.gson.TypeAdapter.fromJson(TypeAdapter.java:260)
       at com.auth0.android.request.internal.SimpleRequest.onResponse(SimpleRequest.java:76)
       at com.squareup.okhttp.Call$AsyncCall.execute(Call.java:177)
       at com.squareup.okhttp.internal.NamedRunnable.run(NamedRunnable.java:33)
       at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1167)
       at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:641)
       at java.lang.Thread.run(Thread.java:764)
    
    Caused by java.net.SocketTimeoutException: timeout
       at okio.SocketAsyncTimeout.newTimeoutException(SocketAsyncTimeout.java:143)
       at okio.AsyncTimeout.access$newTimeoutException(AsyncTimeout.java:162)
       at okio.AsyncTimeout.withTimeout(AsyncTimeout.java:154)
       at okio.AsyncTimeout$source$1.read(AsyncTimeout.java:340)
       at okio.internal.RealBufferedSourceKt.commonRead(RealBufferedSourceKt.java:39)
       at okio.RealBufferedSource.read(RealBufferedSource.java:188)
       at com.squareup.okhttp.internal.http.Http1xStream$ChunkedSource.read(Http1xStream.java:439)
       at okio.internal.RealBufferedSourceKt.commonRead(RealBufferedSourceKt.java:39)
       at okio.RealBufferedSource.read(RealBufferedSource.java:188)
       at okio.internal.RealBufferedSourceKt.commonExhausted(RealBufferedSourceKt.java:49)
       at okio.RealBufferedSource.exhausted(RealBufferedSource.java:198)
       at okio.InflaterSource.refill(InflaterSource.java:112)
       at okio.InflaterSource.readOrInflate(InflaterSource.java:76)
       at okio.InflaterSource.read(InflaterSource.java:49)
       at okio.GzipSource.read(GzipSource.java:69)
       at okio.RealBufferedSource$inputStream$1.read(RealBufferedSource.java:158)
       at sun.nio.cs.StreamDecoder.readBytes(StreamDecoder.java:288)
       at sun.nio.cs.StreamDecoder.implRead(StreamDecoder.java:351)
       at sun.nio.cs.StreamDecoder.read(StreamDecoder.java:180)
       at java.io.InputStreamReader.read(InputStreamReader.java:184)
       at com.google.gson.stream.JsonReader.fillBuffer(JsonReader.java:1291)
       at com.google.gson.stream.JsonReader.nextNonWhitespace(JsonReader.java:1329)
       at com.google.gson.stream.JsonReader.doPeek(JsonReader.java:550)
       at com.google.gson.stream.JsonReader.peek(JsonReader.java:426)
       at com.google.gson.internal.bind.TypeAdapters$29.read(TypeAdapters.java:700)
       at com.google.gson.internal.bind.TypeAdapters$29.read(TypeAdapters.java:723)
       at com.google.gson.internal.bind.TypeAdapters$29.read(TypeAdapters.java:698)
       at com.google.gson.internal.Streams.parse(Streams.java:48)
       at com.google.gson.internal.bind.TreeTypeAdapter.read(TreeTypeAdapter.java:65)
       at com.google.gson.TypeAdapter.fromJson(TypeAdapter.java:260)
       at com.auth0.android.request.internal.SimpleRequest.onResponse(SimpleRequest.java:76)
       at com.squareup.okhttp.Call$AsyncCall.execute(Call.java:177)
       at com.squareup.okhttp.internal.NamedRunnable.run(NamedRunnable.java:33)
       at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1167)
       at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:641)
       at java.lang.Thread.run(Thread.java:764)
    ```

--- auth0/src/main/java/com/auth0/android/request/internal/SimpleRequest.java
+++ auth0/src/main/java/com/auth0/android/request/internal/SimpleRequest.java
@@ -32,6 +32,7 @@ import com.auth0.android.RequestBodyBuildException;
 import com.auth0.android.request.ErrorBuilder;
 import com.auth0.android.request.ParameterizableRequest;
 import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import com.google.gson.reflect.TypeToken;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.HttpUrl;
@@ -77,7 +78,7 @@ class SimpleRequest<T, U extends Auth0Exception> extends BaseRequest<T, U> imple
             Reader charStream = body.charStream();
             T payload = getAdapter().fromJson(charStream);
             postOnSuccess(payload);
-        } catch (IOException e) {
+        } catch (IOException | JsonIOException e) {
             final Auth0Exception auth0Exception = new Auth0Exception("Failed to parse response to request to " + url, e);
             postOnFailure(getErrorBuilder().from("Failed to parse a successful response", auth0Exception));
         } finally {
