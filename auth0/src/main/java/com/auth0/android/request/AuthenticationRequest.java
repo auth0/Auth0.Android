15602dc6e1e57343f0c74a1091edc99a07d92943
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-12-10 18:47:45 +0100

    More String-String request changes. Document them in the migration guide.

--- auth0/src/main/java/com/auth0/android/request/AuthenticationRequest.java
+++ auth0/src/main/java/com/auth0/android/request/AuthenticationRequest.java
@@ -73,6 +73,6 @@ public interface AuthenticationRequest extends Request<Credentials, Authenticati
      * @return itself
      */
     @NonNull
-    AuthenticationRequest addAuthenticationParameters(@NonNull Map<String, Object> parameters);
+    AuthenticationRequest addAuthenticationParameters(@NonNull Map<String, String> parameters);
 
 }
