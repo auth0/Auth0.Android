283a9e6ae8aeeebc5716f3644937f9300ac0a27f
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-12-14 20:57:02 +0100

    Remove unused legacy path check and fix tests

--- auth0/src/main/java/com/auth0/android/request/internal/BaseAuthenticationRequest.java
+++ auth0/src/main/java/com/auth0/android/request/internal/BaseAuthenticationRequest.java
@@ -1,7 +1,5 @@
 package com.auth0.android.request.internal;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.auth0.android.authentication.AuthenticationException;
@@ -11,7 +9,6 @@ import com.google.gson.Gson;
 import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static com.auth0.android.authentication.ParameterBuilder.AUDIENCE_KEY;
@@ -23,14 +20,8 @@ import static com.auth0.android.authentication.ParameterBuilder.SCOPE_KEY;
 
 class BaseAuthenticationRequest extends SimpleRequest<Credentials, AuthenticationException> implements AuthenticationRequest {
 
-    private static final String TAG = BaseAuthenticationRequest.class.getSimpleName();
-
     public BaseAuthenticationRequest(HttpUrl url, OkHttpClient client, Gson gson, String httpMethod) {
         super(url, client, gson, httpMethod, Credentials.class, new AuthenticationErrorBuilder());
-    }
-
-    private boolean hasLegacyPath() {
-        return !url.encodedPath().equals("/oauth/token");
     }
 
     /**
@@ -55,10 +46,6 @@ class BaseAuthenticationRequest extends SimpleRequest<Credentials, Authenticatio
     @NonNull
     @Override
     public AuthenticationRequest setConnection(@NonNull String connection) {
-        if (!hasLegacyPath()) {
-            Log.w(TAG, "Not setting the 'connection' parameter as the request is using a OAuth 2.0 API Authorization endpoint that doesn't support it.");
-            return this;
-        }
         addParameter(CONNECTION_KEY, connection);
         return this;
     }
@@ -72,10 +59,6 @@ class BaseAuthenticationRequest extends SimpleRequest<Credentials, Authenticatio
     @NonNull
     @Override
     public AuthenticationRequest setRealm(@NonNull String realm) {
-        if (hasLegacyPath()) {
-            Log.w(TAG, "Not setting the 'realm' parameter as the request is using a Legacy Authorization API endpoint that doesn't support it.");
-            return this;
-        }
         addParameter(REALM_KEY, realm);
         return this;
     }
@@ -120,20 +103,13 @@ class BaseAuthenticationRequest extends SimpleRequest<Credentials, Authenticatio
     @NonNull
     @Override
     public AuthenticationRequest addAuthenticationParameters(@NonNull Map<String, Object> parameters) {
-        final HashMap<String, Object> params = new HashMap<>(parameters);
-        if (parameters.containsKey(CONNECTION_KEY)) {
-            setConnection((String) params.remove(CONNECTION_KEY));
-        }
-        if (parameters.containsKey(REALM_KEY)) {
-            setRealm((String) params.remove(REALM_KEY));
-        }
-        addParameters(params);
+        addParameters(parameters);
         return this;
     }
 
     @NonNull
     @Override
-    public BaseAuthenticationRequest addHeader(@NonNull String name, @NonNull String value) {
+    public AuthenticationRequest addHeader(@NonNull String name, @NonNull String value) {
         super.addHeader(name, value);
         return this;
     }
