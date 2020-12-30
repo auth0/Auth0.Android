ba9ef068d644df0d9a5d8d11d236613ef5273d46
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-12-30 15:59:20 +0100

    Migrate Auth0 and MockAuth0

--- auth0/src/test/java/com/auth0/android/MockAuth0.kt
+++ auth0/src/test/java/com/auth0/android/MockAuth0.kt
@@ -1,41 +1,31 @@
-package com.auth0.android;
+package com.auth0.android
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.HttpUrl
 
 /**
- * Mock implementation of {@linkplain Auth0} for tests.
+ * Mock implementation of [Auth0] for tests.
  * This implementation does not enforce HTTPS, and should be removed after tests are able
  * to run with the MockWebServer using HTTPS.
  */
-public class MockAuth0 extends Auth0 {
-
-    public MockAuth0(@NonNull String clientId, @NonNull String domain) {
-        this(clientId, domain, null);
-    }
-
-    public MockAuth0(@NonNull String clientId, @NonNull String domain, @Nullable String configurationDomain) {
-        super(clientId, domain, configurationDomain);
-    }
-
+internal class MockAuth0 @JvmOverloads constructor(
+    clientId: String,
+    domain: String,
+    configurationDomain: String? = null
+) : Auth0(clientId, domain, configurationDomain) {
     /**
-     * Returns the result of calling of {@linkplain HttpUrl#parse(String)} on the provided string.
+     * Returns the result of calling of [HttpUrl.parse] on the provided string.
      * Overriden to not enforce HTTPS for tests.
      *
      * @param url The URL to parse
-     * @return The parsed URL, or null if the {@code url} parameter was null.
+     * @return The parsed URL, or null if the `url` parameter was null.
      */
-    @Override
-    HttpUrl ensureValidUrl(String url) {
+    override fun ensureValidUrl(url: String?): HttpUrl? {
         /*
         TODO [SDK-2221]: get MockWebServer running with HTTPS and remove this class. Should be done
          after updating to more recent versions of OkHttp
          */
-        if (url == null) {
-            return null;
-        }
-        return HttpUrl.parse(url);
+        return if (url == null) {
+            null
+        } else HttpUrl.parse(url)
     }
 }
