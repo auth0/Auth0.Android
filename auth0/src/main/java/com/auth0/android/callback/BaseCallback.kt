e8e303fd40bbf5ac2d367565d0445ab99005ef33
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-01-05 20:34:21 +0100

    Fix javadocs and leftover implementations

--- auth0/src/main/java/com/auth0/android/callback/BaseCallback.kt
+++ auth0/src/main/java/com/auth0/android/callback/BaseCallback.kt
@@ -31,9 +31,9 @@ import com.auth0.android.Auth0Exception
 //TODO [SDK-2185]: Merge this interface into Callback
 public interface BaseCallback<T, U : Auth0Exception> : Callback<U> {
     /**
-     * Method called on success with the payload or null.
+     * Method called on success with the payload.
      *
-     * @param payload Request payload or null
+     * @param payload Request payload
      */
     public fun onSuccess(payload: T)
 }
