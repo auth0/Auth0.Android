ea612cd75c29e820f76eae3e3838960ad0aae204
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-01-13 23:16:50 +0100

    Make GsonProvider expose a singleton

--- auth0/src/main/java/com/auth0/android/request/internal/GsonProvider.kt
+++ auth0/src/main/java/com/auth0/android/request/internal/GsonProvider.kt
@@ -1,43 +1,40 @@
-package com.auth0.android.request.internal;
+package com.auth0.android.request.internal
 
-import androidx.annotation.NonNull;
-import androidx.annotation.VisibleForTesting;
+import androidx.annotation.VisibleForTesting
+import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
+import com.auth0.android.util.JsonRequiredTypeAdapterFactory
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import java.security.PublicKey
+import java.text.SimpleDateFormat
+import java.util.*
 
-import com.auth0.android.result.Credentials;
-import com.auth0.android.result.UserProfile;
-import com.auth0.android.util.JsonRequiredTypeAdapterFactory;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+internal object GsonProvider {
+    internal val gson: Gson
+    private var sdf: SimpleDateFormat
+    private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 
-import java.lang.reflect.Type;
-import java.security.PublicKey;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Map;
-
-public abstract class GsonProvider {
-
-    static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-
-    @NonNull
-    public static Gson buildGson() {
-        Type jwksType = new TypeToken<Map<String, PublicKey>>() {
-        }.getType();
-
-        return new GsonBuilder()
-                .registerTypeAdapterFactory(new JsonRequiredTypeAdapterFactory())
-                .registerTypeAdapter(UserProfile.class, new UserProfileDeserializer())
-                .registerTypeAdapter(Credentials.class, new CredentialsDeserializer())
-                .registerTypeAdapter(jwksType, new JwksDeserializer())
-                .setDateFormat(DATE_FORMAT)
-                .create();
+    init {
+        val jwksType = TypeToken.getParameterized(
+            Map::class.java,
+            String::class.java,
+            PublicKey::class.java
+        ).type
+        gson = GsonBuilder()
+            .registerTypeAdapterFactory(JsonRequiredTypeAdapterFactory())
+            .registerTypeAdapter(UserProfile::class.java, UserProfileDeserializer())
+            .registerTypeAdapter(Credentials::class.java, CredentialsDeserializer())
+            .registerTypeAdapter(jwksType, JwksDeserializer())
+            .setDateFormat(DATE_FORMAT)
+            .create()
+        sdf = SimpleDateFormat(DATE_FORMAT, Locale.US)
     }
 
+    @JvmStatic
     @VisibleForTesting
-    static String formatDate(Date date) {
-        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT, Locale.US);
-        return sdf.format(date);
+    fun formatDate(date: Date): String {
+        return sdf.format(date)
     }
 }
