bb5f84e1ace6c55835f677c9f973de2b10951daa
James Anderson <jim.anderson@auth0.com>
2021-01-15 15:12:25 -0600

    Update licenses

--- auth0/src/test/java/com/auth0/android/authentication/AuthenticationAPIClientTest.java
+++ auth0/src/test/java/com/auth0/android/authentication/AuthenticationAPIClientTest.java
@@ -1,27 +1,3 @@
-/*
- * AuthenticationAPIClientTest.java
- *
- * Copyright (c) 2015 Auth0 (http://auth0.com)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.auth0.android.authentication;
 
 
@@ -47,8 +23,6 @@ import com.auth0.android.util.MockAuthenticationCallback;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import okhttp3.HttpUrl;
-import okhttp3.mockwebserver.RecordedRequest;
 
 import org.hamcrest.collection.IsMapContaining;
 import org.junit.After;
@@ -69,6 +43,9 @@ import java.security.PublicKey;
 import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
+
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.RecordedRequest;
 
 import static com.auth0.android.util.AuthenticationCallbackMatcher.hasError;
 import static com.auth0.android.util.AuthenticationCallbackMatcher.hasNoError;
