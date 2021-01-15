39b856e1015e7e23cf0ef199799c159acde97a70
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-01-15 18:25:36 +0100

    remove Credentials constructor that took an expires in

--- auth0/src/main/java/com/auth0/android/result/Credentials.kt
+++ auth0/src/main/java/com/auth0/android/result/Credentials.kt
@@ -11,12 +11,11 @@ import java.util.*
  *  * *accessToken*: Access Token for Auth0 API
  *  * *refreshToken*: Refresh Token that can be used to request new tokens without signing in again
  *  * *type*: The type of the received Token.
- *  * *expiresIn*: The token lifetime in seconds.
  *  * *expiresAt*: The token expiration date.
  *  * *scope*: The token's granted scope.
  *
  */
-public open class Credentials private constructor(
+public open class Credentials(
     /**
      * Getter for the Identity Token with user information.
      *
@@ -35,16 +34,25 @@ public open class Credentials private constructor(
      * @return the token type.
      */
     @field:SerializedName("token_type") public val type: String?,
+
     /**
      * Getter for the Refresh Token that can be used to request new tokens without signing in again.
      *
      * @return the Refresh Token.
      */
     @field:SerializedName("refresh_token") public val refreshToken: String?,
-    expiresIn: Long?,
-    expiresAt: Date?,
+
     /**
-     * Getter for the token's granted scope. Only available if the requested scope differs from the granted one.
+     * Getter for the expiration date of the Access Token.
+     * Once expired, the Access Token can no longer be used to access an API and a new Access Token needs to be obtained.
+     *
+     * @return the expiration date of this Access Token
+     */
+    @SerializedName("expires_at")
+    public val expiresAt: Date?,
+
+    /**
+     * Getter for the access token's granted scope. Only available if the requested scope differs from the granted one.
      *
      * @return the granted scope.
      */
@@ -53,56 +61,7 @@ public open class Credentials private constructor(
     ) public val scope: String?
 ) {
 
-    /**
-     * Getter for the token lifetime in seconds.
-     * Once expired, the token can no longer be used to access an API and a new token needs to be obtained.
-     *
-     * @return the token lifetime in seconds.
-     */
-    @SerializedName("expires_in")
-    public var expiresIn: Long? = null
-
-    /**
-     * Getter for the expiration date of this token.
-     * Once expired, the token can no longer be used to access an API and a new token needs to be obtained.
-     *
-     * @return the expiration date of this token
-     */
-    @SerializedName("expires_at")
-    public var expiresAt: Date? = null
-
-    //TODO [SDK-1431]: Deprecate this constructor
-    public constructor(
-        idToken: String?,
-        accessToken: String?,
-        type: String?,
-        refreshToken: String?,
-        expiresIn: Long?
-    ) : this(idToken, accessToken, type, refreshToken, expiresIn, null, null)
-
-    public constructor(
-        idToken: String?,
-        accessToken: String?,
-        type: String?,
-        refreshToken: String?,
-        expiresAt: Date?,
-        scope: String?
-    ) : this(idToken, accessToken, type, refreshToken, null, expiresAt, scope)
-
     @get:VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     internal open val currentTimeInMillis: Long
         get() = System.currentTimeMillis()
-
-    init {
-        if (expiresAt == null && expiresIn != null) {
-            this.expiresAt = Date(currentTimeInMillis + expiresIn * 1000)
-        } else {
-            this.expiresAt = expiresAt
-        }
-        this.expiresIn = if (expiresIn == null && expiresAt != null) {
-            (expiresAt.time - currentTimeInMillis) / 1000
-        } else {
-            expiresIn
-        }
-    }
 }
--- auth0/src/test/java/com/auth0/android/result/CredentialsTest.java
+++ auth0/src/test/java/com/auth0/android/result/CredentialsTest.java
@@ -1,43 +1,24 @@
 package com.auth0.android.result;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.closeTo;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 
 public class CredentialsTest {
 
     @Test
-    public void shouldCreateWithExpiresAtDateAndSetExpiresIn() {
+    public void shouldCreate() {
         Date date = new Date();
-        long expiresIn = (date.getTime() - CredentialsMock.CURRENT_TIME_MS) / 1000;
         Credentials credentials = new CredentialsMock("idToken", "accessToken", "type", "refreshToken", date, "scope");
         assertThat(credentials.getIdToken(), is("idToken"));
         assertThat(credentials.getAccessToken(), is("accessToken"));
         assertThat(credentials.getType(), is("type"));
         assertThat(credentials.getRefreshToken(), is("refreshToken"));
-        assertThat(credentials.getExpiresIn(), is(notNullValue()));
-        assertThat(credentials.getExpiresIn().doubleValue(), CoreMatchers.is(closeTo(expiresIn, 1)));
         assertThat(credentials.getExpiresAt(), is(date));
         assertThat(credentials.getScope(), is("scope"));
-    }
-
-    @Test
-    public void shouldCreateWithExpiresInAndSetExpiresAt() {
-        Credentials credentials = new CredentialsMock("idToken", "accessToken", "type", "refreshToken", 86400L);
-        assertThat(credentials.getIdToken(), is("idToken"));
-        assertThat(credentials.getAccessToken(), is("accessToken"));
-        assertThat(credentials.getType(), is("type"));
-        assertThat(credentials.getRefreshToken(), is("refreshToken"));
-        assertThat(credentials.getExpiresIn(), is(notNullValue()));
-        assertThat(credentials.getExpiresIn().doubleValue(), CoreMatchers.is(closeTo(86400, 1)));
-        Date expirationDate = new Date(CredentialsMock.CURRENT_TIME_MS + 86400L * 1000);
-        assertThat(credentials.getExpiresAt(), is(expirationDate));
     }
 
     @Test
