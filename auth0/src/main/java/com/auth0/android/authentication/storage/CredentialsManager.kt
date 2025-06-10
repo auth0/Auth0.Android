7521d105003056924768f3a8c840c92fb11bf8e7
Nandan Prabhu <nandan.prabhup@okta.com>
2025-06-10 11:05:10 +0530

    GH issue Allow accessing credentials without an attempt to refresh #378

--- auth0/src/main/java/com/auth0/android/authentication/storage/CredentialsManager.kt
+++ auth0/src/main/java/com/auth0/android/authentication/storage/CredentialsManager.kt
@@ -1,21 +1,28 @@
 package com.auth0.android.authentication.storage
 
 import android.text.TextUtils
+import android.util.Base64
 import android.util.Log
 import androidx.annotation.VisibleForTesting
 import com.auth0.android.authentication.AuthenticationAPIClient
 import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.authentication.storage.SecureCredentialsManager.Companion.KEY_CREDENTIALS
 import com.auth0.android.callback.Callback
 import com.auth0.android.request.internal.GsonProvider
+import com.auth0.android.request.internal.Jwt
 import com.auth0.android.result.APICredentials
 import com.auth0.android.result.Credentials
+import com.auth0.android.result.OptionalCredentials
 import com.auth0.android.result.SSOCredentials
+import com.auth0.android.result.UserProfile
 import com.auth0.android.result.toAPICredentials
 import com.google.gson.Gson
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.util.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import kotlin.collections.component1
+import kotlin.collections.component2
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
@@ -43,6 +50,47 @@ public class CredentialsManager @VisibleForTesting(otherwise = VisibleForTesting
         JWTDecoder(),
         Executors.newSingleThreadExecutor()
     )
+
+    private fun retrieveCredentials() : Credentials? {
+        val encryptedEncoded = storage.retrieveString(KEY_CREDENTIALS)
+        if (encryptedEncoded.isNullOrBlank()) {
+            return null
+        }
+        val encrypted = android.util.Base64.decode(encryptedEncoded, Base64.DEFAULT)
+        val json: String
+        try {
+            json = String(crypto.decrypt(encrypted))
+        } catch (e: IncompatibleDeviceException) {
+            return null
+        } catch (e: CryptoException) {
+            return null
+        }
+        val bridgeCredentials = gson.fromJson(json, OptionalCredentials::class.java)/* OPTIONAL CREDENTIALS
+             * This bridge is required to prevent users from being logged out when
+             * migrating from Credentials with optional Access Token and ID token
+             */
+        val credentials = Credentials(
+            bridgeCredentials.idToken.orEmpty(),
+            bridgeCredentials.accessToken.orEmpty(),
+            bridgeCredentials.type.orEmpty(),
+            bridgeCredentials.refreshToken,
+            bridgeCredentials.expiresAt ?: Date(),
+            bridgeCredentials.scope
+        )
+        return credentials
+    }
+
+    public override val userProfile: UserProfile?
+        get() {
+            val credentials: Credentials? = retrieveCredentials()
+            // Handle null credentials gracefully
+            if (credentials == null) {
+                return null
+            }
+            val (_, payload) = Jwt.splitToken(credentials.idToken)
+            val gson = GsonProvider.gson
+            return gson.fromJson(Jwt.decodeBase64(payload), UserProfile::class.java)
+        }
 
     /**
      * Stores the given credentials in the storage. Must have an access_token or id_token and a expires_in value.
